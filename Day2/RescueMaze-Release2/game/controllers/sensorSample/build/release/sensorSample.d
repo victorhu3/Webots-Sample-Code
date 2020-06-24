161c57f48b9e6bdfb1f1614f1e0be99f3889bc5f
Victor Hu <victorhu3@gmail.com>
2020-06-24 13:29:00 -0400

    Removed intermediate build files

--- Day2/RescueMaze-Release2/game/controllers/sensorSample/build/release/sensorSample.d
+++
@@ -1,8 +0,0 @@
-build/release/sensorSample.o: sensorSample.cpp \
- /usr/local/webots/include/controller/cpp/webots/Robot.hpp \
- /usr/local/webots/include/controller/cpp/webots/DistanceSensor.hpp \
- /usr/local/webots/include/controller/cpp/webots/Device.hpp \
- /usr/local/webots/include/controller/cpp/webots/../../c/webots/types.h \
- /usr/local/webots/include/controller/cpp/webots/Gyro.hpp \
- /usr/local/webots/include/controller/cpp/webots/GPS.hpp \
- /usr/local/webots/include/controller/cpp/webots/LightSensor.hpp
